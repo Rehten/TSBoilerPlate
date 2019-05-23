2ea8f734478b56302cccf5146af67a079d9f7cd6
rehten <Sithas@yandex.ru>
2019-05-23 17:27:42 +0300

    page 136

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { TestScheduler } from 'rxjs/testing'
 import './index123.js'
-
-let scheduler: TestScheduler = new TestScheduler((actual, expected) => {
-  console.log(123)
-})
-
-console.log(scheduler)
--- src/index123.js
+++ src/index123.js
@@ -1,1 +1,36 @@
-console.log('Abibasta')
+class Observer {
+  constructor(dataFn) {
+    this.dataFn = dataFn
+  }
+
+  next (value) {
+    this.dataFn(value)
+  }
+}
+
+class Observable {
+  static create (behaviourFn) {
+    return new Observable(behaviourFn)
+  }
+
+  constructor (behaviourFn) {
+    this.behaviourFn = behaviourFn
+  }
+
+  subscribe (dataFn) {
+    let observer = new Observer(dataFn)
+
+    this.behaviourFn(observer)
+  }
+}
+
+let stream$ = Observable.create(observer => {
+  let count = 0
+  let max = 56
+
+  while (count < 56) {
+    observer.next(count++)
+  }
+})
+
+stream$.subscribe(value => console.log(value))
